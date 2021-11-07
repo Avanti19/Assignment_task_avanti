7372dfbebb98325bcecf7095651223506c273587
Avanti19 <49921401+Avanti19@users.noreply.github.com>
2021-11-07 20:48:47 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "access_key" {
+description = "AWS Access key"
+default = "<your access keys>"
+}
+
+variable "secret_key" {
+description = "AWS Secret Key"
+default = "<your secret keys>"
+}
+
+
+variable "region" {
+description = "AWS region for hosting your network"
+default = "ap-south-1"
+}
